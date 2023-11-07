d8c45b6a2179b2605aa4d2581eadd8c085862e01
@KAMESH <kamesh1209s@gmail.com>
2023-11-07 21:45:57 +0530

    Update main.py
    clearly defined function with comments

--- main.py
+++ main.py
@@ -7,22 +7,35 @@ print("""
                                          
 """)
 
+# Import necessary libraries
 import cv2
 import numpy as np
 import face_recognition
 import os
 
+# Define the directory where your images are stored
 path = "faces"
-images =[]
+
+# Create lists to store images, class names, and face encodings
+images = []
 classnames = []
+
+# Get the list of image files in the directory
 mylist = os.listdir(path)
-print(mylist)
+
+# Print the list of image files for reference
+print("List of image files:", mylist)
+
+# Loop through the images and store them in the 'images' list, along with class names
 for cl in mylist:
     curimg = cv2.imread(f"{path}/{cl}")
     images.append(curimg)
     classnames.append(os.path.splitext(cl)[0])
-print(classnames)
 
+# Print the class names for reference
+print("Class names:", classnames)
+
+# Function to find face encodings for a list of images
 def find_encodings(images):
     encodelist = []
     for img in images:
@@ -31,42 +44,54 @@ def find_encodings(images):
         encodelist.append(encode)
     return encodelist
 
+# Get the known face encodings
 encodelistknow = find_encodings(images)
-#print(len(encodelistknow))
-print("Encodeing completed..........")
+print("Encoding completed...")
 
-cap = cv2.VideoCapture(0) # if you can see anything try '1' instead of '0'
+# Open the webcam (you can change the argument to '1' if '0' doesn't work)
+cap = cv2.VideoCapture(0)
 
 while True:
     success, img = cap.read()
-    imgs = cv2.resize(img, (0,0),None,0.25,0.25)
+
+    # Resize the captured frame for faster processing
+    imgs = cv2.resize(img, (0, 0), None, 0.25, 0.25)
     imgs = cv2.cvtColor(imgs, cv2.COLOR_BGR2RGB)
 
+    # Find face locations and encodings in the current frame
     facecurframe = face_recognition.face_locations(imgs)
-    encodecurframe = face_recognition.face_encodings(imgs,facecurframe)
+    encodecurframe = face_recognition.face_encodings(imgs, facecurframe)
 
-    for encodeface, faceloc in zip(encodecurframe,facecurframe):
+    for encodeface, faceloc in zip(encodecurframe, facecurframe):
+        # Compare face encodings with known encodings
         macthes = face_recognition.compare_faces(encodelistknow, encodeface)
         facedis = face_recognition.face_distance(encodelistknow, encodeface)
-        #print(facedis)
         matchindex = np.argmin(facedis)
-        
 
         if macthes[matchindex]:
+            # Get the name of the recognized person
             name = classnames[matchindex].upper()
-            #print(name)
-            y1,x2,y2,x1 = faceloc
-            y1,x2,y2,x1 = y1*4,x2*4,y2*4,x1*4
+            y1, x2, y2, x1 = faceloc
 
-            cv2.rectangle(img,(x1,y1),(x1,y2),(0,255,0),2)
-            cv2.rectangle(img, (x1,y2), (x2,y2), (0,255,0), cv2.FILLED)
-            #cv2.rectangle(img, (x1,y2-35), (x2,y2), (0,255,0), cv2.FILLED)
-            #cv2.putText(img, name, (x1+6,y2-6), cv2.FONT_HERSHEY_COMPLEX,3, (255,255,255),2)
-            cv2.putText(img, name, (x1,y2), cv2.FONT_HERSHEY_COMPLEX,1, (255,255,255),2)
-            
+            # Scale the coordinates for the original frame
+            y1, x2, y2, x1 = y1 * 4, x2 * 4, y2 * 4, x1 * 4
 
+            # Draw a rectangle around the recognized face
+            cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
+            cv2.rectangle(img, (x1, y2 - 35), (x2, y2), (0, 255, 0), cv2.FILLED)
 
-    cv2.imshow("RECO", img)
-    cv2.waitKey(1)
+            # Display the name of the recognized person
+            cv2.putText(img, name, (x1 + 6, y2 - 6), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
+
+    # Display the frame with recognized faces
+    cv2.imshow("Face Recognition", img)
+    
+    # Exit the loop when the 'q' key is pressed
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Release the webcam and close all OpenCV windows
+cap.release()
+cv2.destroyAllWindows()
 
 
